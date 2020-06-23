314d4d9b1b1234b3c7682df66e91bc31e9f731c4
Andre <3oliva41@solent.ac.uk>
2020-06-23 09:10:46 +0100

    Add project files.

---
+++ BugHero/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugHero.Startup))]
+namespace BugHero
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
