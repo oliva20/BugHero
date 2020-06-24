21df956e0be15cd094b31091d8534f69e2c826ad
Andre <3oliva41@solent.ac.uk>
2020-06-24 09:56:25 +0100

    Added interface for the repository and the implementation class. ** not working.

---
+++ DataAccessLayer/GenericDataRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer
+{
+    internal class GenericDataRepository<T> : IGenericDataRepository<T> where T : class
+    {
+        public virtual void Add(params T[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            List<T> list;
+            
+            throw new NotImplementedException();
+        }
+
+        public virtual IList<T> GetList(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual T GetSingle(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(params T[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Update(params T[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
